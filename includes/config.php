57d900c122e22d61611c3311977cfd35cd9bb218
buithuyquyen0397-spec <buithuyquyen0397@gmail.com>
2025-12-20 19:36:48 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,14 @@
 <?php
-// Cấu hình database cho XAMPP trên macOS
-// XAMPP mặc định sử dụng MySQL với các thông tin sau:
-define('DB_HOST', '127.0.0.1');  // hoặc '127.0.0.1'
-define('DB_USER', 'root');       // User mặc định của XAMPP
-define('DB_PASS', '');           // Mật khẩu mặc định (trống) hoặc 'root' nếu đã đặt
-define('DB_NAME', 'khachsan');   // Tên database
-define('DB_PORT', '3306');       // Port mặc định của MySQL trong XAMPP
+
+//define('DB_HOST', '127.0.0.1');  
+//define('DB_USER', 'root');      
+//define('DB_PASS', '');           
+//define('DB_NAME', 'khachsan');   
+//define('DB_PORT', '3306');       
+
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASS', getenv('MYSQLPASSWORD'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_PORT', getenv('MYSQLPORT'));
 ?>
